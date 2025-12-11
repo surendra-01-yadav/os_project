eeb8b5e99cb04f0c8f99efc1a7a27803d8ed1793
Shaik Athif <athifshaik2843@gmail.com>
2025-12-11 18:33:26 +0530

    PART 1: MODULE INITIALIZATION  (Commit #1)

--- module_logic.js
+++ module_logic.js
@@ -1,1 +1,22 @@
+const LogicModule = {
+    state: {
+        scenario: 'PC', // 'PC' or 'DP'
+        log: 'System Ready',
 
+        // Producer–Consumer State
+        bufferSize: 5,
+        buffer: [],
+        mutex: 1,
+        empty: 5,
+        full: 0,
+        producers: [{id:1, state:'idle'}, {id:2, state:'idle'}],
+        consumers: [{id:1, state:'idle'}, {id:2, state:'idle'}],
+
+        // Dining Philosophers State
+        philosophers: [],
+        forks: []
+    },
+
+    init: function() { 
+        this.reset(); 
+    },
